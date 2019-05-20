4c5a76fbca55f840f229f1814c9511889f0dd652
caleb <calebplant@aggienetwork.com>
2019-05-20 18:41:18 -0500

    Improved readability of generated QT screens

--- screens/main_window.py
+++ screens/main_window.py
@@ -14,6 +14,11 @@ from screens.screen_functions.stacked_widget_subclass import MyStackWidget
 class Ui_MainWindow(object):
 
     def setupUi(self, MainWindow):
+
+        # =================================================================================
+        #  MAIN WINDOW
+        # =================================================================================
+
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1076, 869)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
@@ -23,6 +28,11 @@ class Ui_MainWindow(object):
         self.stackedWidget = MyStackWidget()
         self.stackedWidget.setFrameShape(QtWidgets.QFrame.Box)
         self.stackedWidget.setObjectName("stackedWidget")
+
+        # =================================================================================
+        #  Start Page
+        # =================================================================================
+
         self.start_page = QtWidgets.QWidget()
         self.start_page.setObjectName("start_page")
         self.gridLayout_4 = QtWidgets.QGridLayout(self.start_page)
@@ -32,12 +42,13 @@ class Ui_MainWindow(object):
         spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.gridLayout.addItem(spacerItem, 0, 1, 1, 1)
 
-        # Start Screen - search button
+        # Search Button
         self.search_btn = QtWidgets.QPushButton(self.start_page)
         self.search_btn.setObjectName("search_btn")
         self.gridLayout.addWidget(self.search_btn, 1, 1, 1, 1)
-        self.search_btn.clicked.connect(self.stackedWidget.go_to_search_screen)
-        
+        # self.search_btn.clicked.connect(self.stackedWidget.go_to_search_screen)
+        self.search_btn.clicked.connect(self.stackedWidget.go_to_tester)
+
         spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.gridLayout.addItem(spacerItem1, 2, 0, 1, 1)
         spacerItem2 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
@@ -45,7 +56,7 @@ class Ui_MainWindow(object):
         spacerItem3 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.gridLayout.addItem(spacerItem3, 2, 2, 1, 1)
 
-        # Start Screen - serialized inventory button
+        # Serialized Inventory Button
         self.serialized_btn = QtWidgets.QPushButton(self.start_page)
         self.serialized_btn.setObjectName("serialized_btn")
         self.gridLayout.addWidget(self.serialized_btn, 3, 1, 1, 1)
@@ -56,12 +67,18 @@ class Ui_MainWindow(object):
         self.gridLayout.addItem(spacerItem4, 4, 1, 1, 1)
         self.gridLayout_4.addLayout(self.gridLayout, 0, 0, 1, 1)
         self.stackedWidget.addWidget(self.start_page)
+
+        # =================================================================================
+        #  Search Results Page
+        # =================================================================================
         self.search_result_page = QtWidgets.QWidget()
         self.search_result_page.setObjectName("search_result_page")
         self.gridLayout_3 = QtWidgets.QGridLayout(self.search_result_page)
         self.gridLayout_3.setObjectName("gridLayout_3")
         self.gridLayout_2 = QtWidgets.QGridLayout()
         self.gridLayout_2.setObjectName("gridLayout_2")
+
+        # Back Button
         self.back_btn_search_result = QtWidgets.QPushButton(self.search_result_page)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
@@ -69,7 +86,11 @@ class Ui_MainWindow(object):
         sizePolicy.setHeightForWidth(self.back_btn_search_result.sizePolicy().hasHeightForWidth())
         self.back_btn_search_result.setSizePolicy(sizePolicy)
         self.back_btn_search_result.setObjectName("back_btn_search_result")
+        self.back_btn_search_result.clicked.connect(self.stackedWidget.go_to_search_screen)
         self.gridLayout_2.addWidget(self.back_btn_search_result, 1, 1, 1, 1)
+
+
+        # Search Result Table
         self.search_result_table = QtWidgets.QTableWidget(self.search_result_page)
         self.search_result_table.setObjectName("search_result_table")
         self.search_result_table.setColumnCount(8)
@@ -90,6 +111,7 @@ class Ui_MainWindow(object):
         self.search_result_table.setHorizontalHeaderItem(6, item)
         item = QtWidgets.QTableWidgetItem()
         self.search_result_table.setHorizontalHeaderItem(7, item)
+
         self.gridLayout_2.addWidget(self.search_result_table, 0, 0, 1, 3)
         spacerItem5 = QtWidgets.QSpacerItem(353, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.gridLayout_2.addItem(spacerItem5, 1, 0, 1, 1)
@@ -97,6 +119,10 @@ class Ui_MainWindow(object):
         self.gridLayout_2.addItem(spacerItem6, 1, 2, 1, 1)
         self.gridLayout_3.addLayout(self.gridLayout_2, 0, 0, 1, 1)
         self.stackedWidget.addWidget(self.search_result_page)
+
+        # =================================================================================
+        #  Search Page
+        # =================================================================================
         self.search_page = QtWidgets.QWidget()
         self.search_page.setObjectName("search_page")
         self.gridLayout_5 = QtWidgets.QGridLayout(self.search_page)
@@ -109,6 +135,8 @@ class Ui_MainWindow(object):
         self.gridLayout_6.addItem(spacerItem8, 1, 0, 1, 1)
         self.gridLayout_7 = QtWidgets.QGridLayout()
         self.gridLayout_7.setObjectName("gridLayout_7")
+
+        # General Search Radio Button
         self.general_radio = QtWidgets.QRadioButton(self.search_page)
         self.general_radio.setText("")
         self.general_radio.setObjectName("general_radio")
@@ -116,43 +144,61 @@ class Ui_MainWindow(object):
         self.label_4 = QtWidgets.QLabel(self.search_page)
         self.label_4.setObjectName("label_4")
         self.gridLayout_7.addWidget(self.label_4, 0, 2, 1, 1, QtCore.Qt.AlignHCenter)
+
+        # Submit Search Button
         self.submit_search_btn = QtWidgets.QPushButton(self.search_page)
         self.submit_search_btn.setObjectName("submit_search_btn")
         self.gridLayout_7.addWidget(self.submit_search_btn, 8, 2, 1, 1)
         self.label_5 = QtWidgets.QLabel(self.search_page)
         self.label_5.setObjectName("label_5")
         self.gridLayout_7.addWidget(self.label_5, 4, 2, 1, 1, QtCore.Qt.AlignHCenter)
+
+        # General Search Field
         self.general_search = QtWidgets.QLineEdit(self.search_page)
         self.general_search.setText("")
         self.general_search.setObjectName("general_search")
         self.gridLayout_7.addWidget(self.general_search, 7, 2, 1, 1)
+
         self.label_6 = QtWidgets.QLabel(self.search_page)
         self.label_6.setObjectName("label_6")
         self.gridLayout_7.addWidget(self.label_6, 6, 2, 1, 1, QtCore.Qt.AlignHCenter)
+
+        # Last Name Search
         self.last_search = QtWidgets.QLineEdit(self.search_page)
         self.last_search.setText("")
         self.last_search.setObjectName("last_search")
         self.gridLayout_7.addWidget(self.last_search, 5, 1, 1, 2)
+
+        # Phone Number Search Field
         self.phone_search = QtWidgets.QLineEdit(self.search_page)
         self.phone_search.setText("")
         self.phone_search.setObjectName("phone_search")
         self.gridLayout_7.addWidget(self.phone_search, 1, 1, 1, 2)
+
+        # Phone Number Radio Button
         self.phone_radio = QtWidgets.QRadioButton(self.search_page)
         self.phone_radio.setText("")
         self.phone_radio.setObjectName("phone_radio")
         self.gridLayout_7.addWidget(self.phone_radio, 1, 0, 1, 1)
+
+        # Last Name Radio Button
         self.last_radio = QtWidgets.QRadioButton(self.search_page)
         self.last_radio.setText("")
         self.last_radio.setObjectName("last_radio")
         self.gridLayout_7.addWidget(self.last_radio, 5, 0, 1, 1)
+
+        # First Name Radio Button
         self.first_radio = QtWidgets.QRadioButton(self.search_page)
         self.first_radio.setText("")
         self.first_radio.setObjectName("first_radio")
         self.gridLayout_7.addWidget(self.first_radio, 3, 0, 1, 1)
+
+        # First Name Search Field
         self.first_search = QtWidgets.QLineEdit(self.search_page)
         self.first_search.setText("")
         self.first_search.setObjectName("first_search")
         self.gridLayout_7.addWidget(self.first_search, 3, 1, 1, 2)
+
         self.label_7 = QtWidgets.QLabel(self.search_page)
         self.label_7.setObjectName("label_7")
         self.gridLayout_7.addWidget(self.label_7, 2, 2, 1, 1, QtCore.Qt.AlignHCenter)
@@ -163,10 +209,18 @@ class Ui_MainWindow(object):
         self.gridLayout_6.addItem(spacerItem10, 2, 1, 1, 1)
         self.gridLayout_5.addLayout(self.gridLayout_6, 0, 0, 1, 1)
         self.stackedWidget.addWidget(self.search_page)
+
+
+        # =================================================================================
+        #  Serialized Inventory Page
+        # =================================================================================
+
         self.serialized_page = QtWidgets.QWidget()
         self.serialized_page.setObjectName("serialized_page")
         self.gridLayout_8 = QtWidgets.QGridLayout(self.serialized_page)
         self.gridLayout_8.setObjectName("gridLayout_8")
+
+        # Serialized Inventory Table
         self.serial_table = QtWidgets.QTableWidget(self.serialized_page)
         self.serial_table.setObjectName("serial_table")
         self.serial_table.setColumnCount(5)
@@ -182,8 +236,11 @@ class Ui_MainWindow(object):
         item = QtWidgets.QTableWidgetItem()
         self.serial_table.setHorizontalHeaderItem(4, item)
         self.gridLayout_8.addWidget(self.serial_table, 0, 0, 1, 3)
+
         spacerItem11 = QtWidgets.QSpacerItem(404, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.gridLayout_8.addItem(spacerItem11, 1, 0, 1, 1)
+
+        # Back Button
         self.back_btn_serial = QtWidgets.QPushButton(self.serialized_page)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
@@ -194,15 +251,22 @@ class Ui_MainWindow(object):
         self.back_btn_serial.setMaximumSize(QtCore.QSize(200, 16777215))
         self.back_btn_serial.setObjectName("back_btn_serial")
         self.gridLayout_8.addWidget(self.back_btn_serial, 1, 1, 1, 1)
+
         spacerItem12 = QtWidgets.QSpacerItem(406, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.gridLayout_8.addItem(spacerItem12, 1, 2, 1, 1)
         self.stackedWidget.addWidget(self.serialized_page)
+
+        # =================================================================================
+        #  Service Request Order (SRO) Page
+        # =================================================================================
         self.sro_page = QtWidgets.QWidget()
         self.sro_page.setObjectName("sro_page")
         self.gridLayout_9 = QtWidgets.QGridLayout(self.sro_page)
         self.gridLayout_9.setObjectName("gridLayout_9")
         self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+
+        # SRO Data Table
         self.data_table_3 = QtWidgets.QTableWidget(self.sro_page)
         self.data_table_3.setEnabled(True)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
@@ -263,17 +327,22 @@ class Ui_MainWindow(object):
         item.setFont(font)
         self.data_table_3.setHorizontalHeaderItem(0, item)
         self.horizontalLayout_3.addWidget(self.data_table_3)
+
         self.verticalLayout_3 = QtWidgets.QVBoxLayout()
         self.verticalLayout_3.setObjectName("verticalLayout_3")
         self.label_8 = QtWidgets.QLabel(self.sro_page)
         self.label_8.setObjectName("label_8")
         self.verticalLayout_3.addWidget(self.label_8)
+
+        # Service Request Notes
         self.service_text = QtWidgets.QTextBrowser(self.sro_page)
         self.service_text.setObjectName("service_text")
         self.verticalLayout_3.addWidget(self.service_text)
         self.label_9 = QtWidgets.QLabel(self.sro_page)
         self.label_9.setObjectName("label_9")
         self.verticalLayout_3.addWidget(self.label_9)
+
+        # Work Done Notes
         self.work_text = QtWidgets.QTextBrowser(self.sro_page)
         self.work_text.setObjectName("work_text")
         self.verticalLayout_3.addWidget(self.work_text)
@@ -293,12 +362,18 @@ class Ui_MainWindow(object):
         spacerItem14 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_4.addItem(spacerItem14)
         self.stackedWidget.addWidget(self.sro_page)
+
+        # =================================================================================
+        #  Customer Page
+        # =================================================================================
         self.customer_page = QtWidgets.QWidget()
         self.customer_page.setObjectName("customer_page")
         self.gridLayout_10 = QtWidgets.QGridLayout(self.customer_page)
         self.gridLayout_10.setObjectName("gridLayout_10")
         spacerItem15 = QtWidgets.QSpacerItem(368, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.gridLayout_10.addItem(spacerItem15, 0, 0, 1, 1)
+
+        # Customer Table 1 - ID/First/Last/Company/Email
         self.customer_table_1 = QtWidgets.QTableWidget(self.customer_page)
         self.customer_table_1.setMaximumSize(QtCore.QSize(251, 231))
         self.customer_table_1.setObjectName("customer_table_1")
@@ -337,10 +412,14 @@ class Ui_MainWindow(object):
         item = QtWidgets.QTableWidgetItem()
         self.customer_table_1.setHorizontalHeaderItem(0, item)
         self.gridLayout_10.addWidget(self.customer_table_1, 0, 1, 1, 1)
+
+
         self.horizontalLayout_7 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_7.setObjectName("horizontalLayout_7")
         self.customer_table_2 = QtWidgets.QTableWidget(self.customer_page)
         self.customer_table_2.setMaximumSize(QtCore.QSize(241, 231))
+
+        # Customer Table 2 - Addr/City/ST/Zip/Last Activity
         self.customer_table_2.setObjectName("customer_table_2")
         self.customer_table_2.setColumnCount(1)
         self.customer_table_2.setRowCount(5)
@@ -377,10 +456,13 @@ class Ui_MainWindow(object):
         item = QtWidgets.QTableWidgetItem()
         self.customer_table_2.setHorizontalHeaderItem(0, item)
         self.horizontalLayout_7.addWidget(self.customer_table_2)
+
+        # Layaway Button
         self.layaway_btn = QtWidgets.QPushButton(self.customer_page)
         self.layaway_btn.setMaximumSize(QtCore.QSize(151, 121))
         self.layaway_btn.setObjectName("layaway_btn")
         self.horizontalLayout_7.addWidget(self.layaway_btn)
+
         spacerItem16 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_7.addItem(spacerItem16)
         self.gridLayout_10.addLayout(self.horizontalLayout_7, 0, 2, 1, 2)
@@ -389,11 +471,14 @@ class Ui_MainWindow(object):
         self.label_10 = QtWidgets.QLabel(self.customer_page)
         self.label_10.setObjectName("label_10")
         self.verticalLayout_6.addWidget(self.label_10)
+
+        # Phone Numbers Table
         self.phone_tbox = QtWidgets.QTableWidget(self.customer_page)
         self.phone_tbox.setMinimumSize(QtCore.QSize(0, 71))
         self.phone_tbox.setObjectName("phone_tbox")
         self.phone_tbox.setColumnCount(0)
         self.phone_tbox.setRowCount(0)
+
         self.verticalLayout_6.addWidget(self.phone_tbox)
         self.gridLayout_10.addLayout(self.verticalLayout_6, 1, 0, 1, 2)
         self.verticalLayout_5 = QtWidgets.QVBoxLayout()
@@ -401,15 +486,20 @@ class Ui_MainWindow(object):
         self.label_12 = QtWidgets.QLabel(self.customer_page)
         self.label_12.setObjectName("label_12")
         self.verticalLayout_5.addWidget(self.label_12)
+
+        # Customer Notes Text Box
         self.notes_tbox = QtWidgets.QTextBrowser(self.customer_page)
         self.notes_tbox.setObjectName("notes_tbox")
         self.verticalLayout_5.addWidget(self.notes_tbox)
         self.gridLayout_10.addLayout(self.verticalLayout_5, 1, 2, 1, 2)
+
         self.verticalLayout_7 = QtWidgets.QVBoxLayout()
         self.verticalLayout_7.setObjectName("verticalLayout_7")
         self.label_13 = QtWidgets.QLabel(self.customer_page)
         self.label_13.setObjectName("label_13")
         self.verticalLayout_7.addWidget(self.label_13)
+
+        # Transactions Table
         self.transactions_table = QtWidgets.QTableWidget(self.customer_page)
         self.transactions_table.setMinimumSize(QtCore.QSize(521, 0))
         self.transactions_table.setObjectName("transactions_table")
@@ -439,6 +529,7 @@ class Ui_MainWindow(object):
         font.setWeight(75)
         item.setFont(font)
         self.transactions_table.setHorizontalHeaderItem(3, item)
+
         self.verticalLayout_7.addWidget(self.transactions_table)
         self.gridLayout_10.addLayout(self.verticalLayout_7, 2, 0, 1, 3)
         self.verticalLayout_4 = QtWidgets.QVBoxLayout()
@@ -446,6 +537,8 @@ class Ui_MainWindow(object):
         self.label_11 = QtWidgets.QLabel(self.customer_page)
         self.label_11.setObjectName("label_11")
         self.verticalLayout_4.addWidget(self.label_11)
+
+        # SRO Table
         self.sro_table = QtWidgets.QTableWidget(self.customer_page)
         self.sro_table.setMaximumSize(QtCore.QSize(401, 16777215))
         self.sro_table.setObjectName("sro_table")
@@ -471,6 +564,7 @@ class Ui_MainWindow(object):
         self.sro_table.setHorizontalHeaderItem(2, item)
         self.verticalLayout_4.addWidget(self.sro_table)
         self.gridLayout_10.addLayout(self.verticalLayout_4, 2, 3, 1, 1)
+
         self.stackedWidget.addWidget(self.customer_page)
         self.horizontalLayout_2.addWidget(self.stackedWidget)
         MainWindow.setCentralWidget(self.centralwidget)
